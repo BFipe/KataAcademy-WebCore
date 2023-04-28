79bf00c5963df840802e207fae05712ee5273685
Mаксимов Илья <ilya.maximov11@gmail.com>
2023-04-28 13:26:47 +0300

    Slider visibility

--- src/js/scroller.js
+++ src/js/scroller.js
@@ -9,18 +9,35 @@
         init = true;
         
         swiper = new Swiper(".swiper-brands", {
-          autoplay: {
-            delay: 3000,
-            disableOnInteraction: false,
-          },
+          // autoplay: {
+          //   delay: 3000,
+          //   disableOnInteraction: false,
+          // },
           direction: "horizontal",
-          slidesPerView: 1,
+          slidesPerView: 2,
           centeredSlides: true,
           spaceBetween: 16,
           pagination: {
             el: ".swiper-pagination",
             clickable: true,
           },
+          breakpoints: {
+            // when window width is >= 320px
+            0: {
+              slidesPerView: 1,
+              spaceBetween: 20
+            },
+            // when window width is >= 480px
+            320: {
+              slidesPerView: 2,
+              spaceBetween: 195
+            },
+            // when window width is >= 640px
+            560: {
+              slidesPerView: 3,
+              spaceBetween: 200
+            }
+          }
         });
       }
     } 
