c4fc4b1d5a936d21adf13f68c26f15260fe96e55
Mаксимов Илья <ilya.maximov11@gmail.com>
2023-04-28 13:30:09 +0300

    final scroller breakpoints for screen

--- src/js/scroller.js
+++ src/js/scroller.js
@@ -9,30 +9,31 @@
         init = true;
         
         swiper = new Swiper(".swiper-brands", {
-          // autoplay: {
-          //   delay: 3000,
-          //   disableOnInteraction: false,
-          // },
+          autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+          },
           direction: "horizontal",
           slidesPerView: 2,
           centeredSlides: true,
+          loop: true,
           spaceBetween: 16,
           pagination: {
             el: ".swiper-pagination",
             clickable: true,
           },
           breakpoints: {
-            // when window width is >= 320px
+            // when window width is >= 0px
             0: {
               slidesPerView: 1,
               spaceBetween: 20
             },
-            // when window width is >= 480px
+            // when window width is >= 320px
             320: {
               slidesPerView: 2,
               spaceBetween: 195
             },
-            // when window width is >= 640px
+            // when window width is >= 560px
             560: {
               slidesPerView: 3,
               spaceBetween: 200
