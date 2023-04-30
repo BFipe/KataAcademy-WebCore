48aaba13b215552b996294d93856c7dc65077950
Mаксимов Илья <ilya.maximov11@gmail.com>
2023-04-30 12:23:39 +0300

    Eslint + Prettier

--- src/js/scroller.js
+++ src/js/scroller.js
@@ -1,54 +1,51 @@
-  let init = false;
-  let pagination = document.querySelector('.swiper-pagination');
-  function swiperCard() {
-    if (window.innerWidth <= 760) {
-      if (!init) {
+let init = false;
+const pagination = document.querySelector('.swiper-pagination');
+function swiperCard() {
+  if (window.innerWidth <= 760) {
+    if (!init) {
+      pagination.classList.remove('hidden');
 
-        pagination.classList.remove('hidden');
+      init = true;
 
-        init = true;
-        
-        swiper = new Swiper(".swiper-brands", {
-          autoplay: {
-            delay: 3000,
-            disableOnInteraction: false,
+      swiper = new Swiper('.swiper-brands', {
+        autoplay: {
+          delay: 3000,
+          disableOnInteraction: false,
+        },
+        direction: 'horizontal',
+        slidesPerView: 2,
+        centeredSlides: true,
+        loop: true,
+        spaceBetween: 16,
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+        },
+        breakpoints: {
+          // when window width is >= 0px
+          0: {
+            slidesPerView: 1,
+            spaceBetween: 20,
           },
-          direction: "horizontal",
-          slidesPerView: 2,
-          centeredSlides: true,
-          loop: true,
-          spaceBetween: 16,
-          pagination: {
-            el: ".swiper-pagination",
-            clickable: true,
+          // when window width is >= 320px
+          320: {
+            slidesPerView: 2,
+            spaceBetween: 195,
           },
-          breakpoints: {
-            // when window width is >= 0px
-            0: {
-              slidesPerView: 1,
-              spaceBetween: 20
-            },
-            // when window width is >= 320px
-            320: {
-              slidesPerView: 2,
-              spaceBetween: 195
-            },
-            // when window width is >= 560px
-            560: {
-              slidesPerView: 3,
-              spaceBetween: 200
-            }
-          }
-        });
-      }
-    } 
-    
-    else if (init) {
-      pagination.classList.add('hidden');
-      swiper.destroy();
-      init = false;
+          // when window width is >= 560px
+          560: {
+            slidesPerView: 3,
+            spaceBetween: 200,
+          },
+        },
+      });
     }
+  } else if (init) {
+    pagination.classList.add('hidden');
+    swiper.destroy();
+    init = false;
   }
-  
-  swiperCard();
-  window.addEventListener("resize", swiperCard);
+}
+
+swiperCard();
+window.addEventListener('resize', swiperCard);
--- src/js/show_more_button.js
+++ src/js/show_more_button.js
@@ -1,50 +1,50 @@
-let logos = document.querySelectorAll(".swiper-slide");
-let button = document.querySelector("#show_more");
+const logos = document.querySelectorAll('.swiper-slide');
+const button = document.querySelector('#show_more');
 let buttonActive = false;
 
 let midscreen;
 
 hideContent(false);
-window.addEventListener("resize", () => hideContent(false));
+window.addEventListener('resize', () => hideContent(false));
 
-button.addEventListener("click", () => {
-    if (!buttonActive) {
-        showAll(logos);
-        button.classList.add('swiper__show-more-button--active');
-        button.textContent = 'Скрыть';
-        buttonActive = true;
-    } else {
-        hideContent(true);
-        button.classList.remove('swiper__show-more-button--active');
-        button.textContent = 'Показать все';
-        buttonActive = false;
-    }
-})
+button.addEventListener('click', () => {
+  if (!buttonActive) {
+    showAll(logos);
+    button.classList.add('swiper__show-more-button--active');
+    button.textContent = 'Скрыть';
+    buttonActive = true;
+  } else {
+    hideContent(true);
+    button.classList.remove('swiper__show-more-button--active');
+    button.textContent = 'Показать все';
+    buttonActive = false;
+  }
+});
 
 function hideContent(isButton) {
-    if ( (!buttonActive || isButton) && window.innerWidth > 760) {
-        midscreen = window.innerWidth > 760 && window.innerWidth <= 1110;
-        
-        if (window.innerWidth > 760 && window.innerWidth < 1110) {
-            hideByNumber(6, logos);
-        } else if (window.innerWidth >= 1110) {
-            hideByNumber(8, logos);
-        }
-    } else {
-        showAll(logos);
+  if ((!buttonActive || isButton) && window.innerWidth > 760) {
+    midscreen = window.innerWidth > 760 && window.innerWidth <= 1110;
+
+    if (window.innerWidth > 760 && window.innerWidth < 1110) {
+      hideByNumber(6, logos);
+    } else if (window.innerWidth >= 1110) {
+      hideByNumber(8, logos);
     }
+  } else {
+    showAll(logos);
+  }
 }
 
 function hideByNumber(number, array) {
-    showAll(logos);
+  showAll(logos);
 
-    for (let i = number; i < array.length; i++) {
-        array[i].classList.add('hidden');
-    }
+  for (let i = number; i < array.length; i++) {
+    array[i].classList.add('hidden');
+  }
 }
 
 function showAll(array) {
-    for (let i = 0; i < array.length; i++) {
-        array[i].classList.remove('hidden');
-    }
+  for (let i = 0; i < array.length; i++) {
+    array[i].classList.remove('hidden');
+  }
 }
